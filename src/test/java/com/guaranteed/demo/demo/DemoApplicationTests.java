4e4c6ac422e977b95f378b77ef457eec06f5a30f
ismael delgado <justridelow@yahoo.com>
2019-10-25 18:41:27 -0500

    init

---
+++ src/test/java/com/guaranteed/demo/demo/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.guaranteed.demo.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
